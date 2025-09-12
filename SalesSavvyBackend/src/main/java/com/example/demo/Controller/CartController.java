f2286bca04f48485da2e1cf0f9b6b8b23e61ef29
Supreeth450 <naiksupreet5@gmail.com>
2025-09-12 13:51:12 +0530

    s

--- SalesSavvyBackend/src/main/java/com/example/demo/Controller/CartController.java
+++ SalesSavvyBackend/src/main/java/com/example/demo/Controller/CartController.java
@@ -3,17 +3,19 @@ package com.example.demo.Controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/cart")
+@CrossOrigin(origins = "http://localhost:5174",allowCredentials = "true")
 public class CartController {
 	
 	
-	@GetMapping
+	@PostMapping("/add")
 	public ResponseEntity<Void> addToCart(@RequestBody Map<String, Object> request){
 		
 		
